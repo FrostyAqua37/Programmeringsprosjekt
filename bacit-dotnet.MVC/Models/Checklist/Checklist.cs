f65acad28d74b0eaa6d38f1b633f6853fd1ba333
krisfollegg <kris.follegg@gmail.com>
2023-11-01 12:23:34 +0100

    La til oversikt og endret skfit

---
+++ bacit-dotnet.MVC/Models/Checklist/Checklist.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bacit_dotnet.MVC.Models.Checklist
+{
+    public class Checklist : Controller
+    {
+        public string? Kategorier { get; set; }
+        public string? Sjekkpunkter { get; set; }
+        public bool OK { get; set; }
+        public bool BørSkiftes { get; set; }
+        public bool Defekt { get; set; }
+    }
+}
+    
+
